27d6c036acdb0416ea8dc3b7b3926a5296635cce
Jack Kavanagh <jackkav@gmail.com>
2018-03-17 18:27:15 +0800

    pluggin stuff together

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react'
 import shortid from 'shortid'
+import { pbParse } from './parsers'
+
 import 'tachyons'
 
 class App extends Component {
@@ -13,27 +15,41 @@ class FlexTable extends Component {
   render() {
     return (
       <div className="pa2">
-        <OuterRow full="A 2018 HDRip XviD AC3-EVO[EtMovies]" />
-        <OuterRow full="B W 2018 HDRip XviD AC3-EVO" />
-        <OuterRow full="T R 2018 ENG HDCAM XViD-AKOAM" />
+        <OuterRow
+          key={shortid.generate()}
+          full="Rade Blunner 2049.HDRip.XviD.AC3-EVO\n\n\t\t\tUploaded 03-23 2016, Size 3.48 GiB, ULed by  condors369 \n\t\t"
+        />
+
+        <OuterRow
+          key={shortid.generate()}
+          full="Sonu Ke Titu Ki Sweety (2018) Hindi 720p DVDRip PRE x264 AAC \n\n\t\t\tUploaded 11-20 04:08, Size 284.93 MiB, ULed by  makintos13 \n\t\t"
+        />
       </div>
     )
   }
 }
 class OuterRow extends Component {
   state = {
-    expanded: true,
+    expanded: false,
+    movie: {},
   }
   open = () => this.setState(p => ({ expanded: !p.expanded }))
+
+  componentWillMount() {
+    const movie = pbParse(this.props.full)
+    this.setState({ movie })
+    console.log(movie)
+  }
+
   render() {
     return (
       <div>
-        <div class="flex dim" onClick={this.open}>
-          <div class="outline w-100 pa3">
-            <code>{this.props.full || 'Not available'}</code>
+        <div className="flex dim" onClick={this.open}>
+          <div className="outline w-100 pa3">
+            <code>{this.state.movie.title || 'Not available'}</code>
           </div>
         </div>
-        {this.state.expanded && <InnerRow />}
+        {this.state.expanded && <InnerRow movie={this.state.movie} />}
       </div>
     )
   }
@@ -41,24 +57,72 @@ class OuterRow extends Component {
 class InnerRow extends Component {
   render() {
     return (
-      <div class="flex flex-wrap items-start">
-        <div class="outline w-1 pa3">
-          <a target="blank" href="https://www.youtube.com/watch?v=2w5VZmos5I4">
-            <img src="https://i.ytimg.com/vi/2w5VZmos5I4/default.jpg" alt="yt" />
-          </a>
+      <div className="flex flex-wrap items-start">
+        <div className="outline w-1 pa3">
+          <Trailer />
         </div>
-        <div class="outline w-1 pa3">
-          <div>Released: 12-12-12</div>
+        <div className="outline w-1 pa3">
+          <div>Released: {this.props.movie.uploadedAt.slice(0, 10)}</div>
           <div>Position: #1</div>
-          <div>Quality: CAM</div>
-          <div>IMDB: 7.7</div>
-          <div>Metascore: 7.7</div>
+          <div>Quality: {this.props.movie.quality}</div>
         </div>
-        <div class="outline w-1 pa3">
-          <div>Actors: Someone Something</div>
-          <div>Description: Lorum ipsum dora sit Lorum ipsum dora sit</div>
+        <div className="outline w-1 pa3">
+          <Scores movie={this.props.movie} />
         </div>
       </div>
     )
   }
+}
+class Scores extends Component {
+  state = {
+    isFetching: false,
+    imdbRating: '',
+  }
+
+  async componentWillMount() {
+    console.log('t')
+    this.setState({ isFetching: true })
+    const { imdbRating, Actors, Metascore, Genre, Plot } = await getOmdb(
+      this.props.movie.movieTitle,
+      this.props.movie.year
+    )
+    this.setState({ isFetching: false, imdbRating, Actors, Metascore, Genre, Plot })
+  }
+
+  render() {
+    if (this.state.isFetching) return <div>loading</div>
+    return (
+      <div>
+        <div>IMDB: {this.state.imdbRating}</div>
+        <div>Metascore: {this.state.Metascore}</div>
+        <div>Genre: {this.state.Genre}</div>
+        <div>Actors: {this.state.Actors}</div>
+        <div>Plot: {this.state.Plot}</div>
+      </div>
+    )
+  }
+}
+
+class Trailer extends Component {
+  state = {
+    isFetching: false,
+  }
+  render() {
+    if (this.state.isFetching) return <div>loading</div>
+    return (
+      <a target="blank" href="https://www.youtube.com/watch?v=2w5VZmos5I4">
+        <img src="https://i.ytimg.com/vi/2w5VZmos5I4/default.jpg" alt="yt" />
+      </a>
+    )
+  }
+}
+const getOmdb = async (name, year) => {
+  const url = `http://www.omdbapi.com/?apikey=6cf170d0&t=${name}&y=${year}`
+  let f = await fetch(url)
+  if (!f.response) {
+    console.log(f.Error, name, year)
+  }
+  let json = await f.json()
+  console.log('l', json)
+  return json
 }
---
+++ src/parsers.js
@@ -0,0 +1,96 @@
+import moment from 'moment'
+
+export const pbParse = input => {
+  if (!input) return false
+  const size =
+    input.match(/Size (.*?), ULed/) &&
+    input.match(/Size (.*?), ULed/).length > 1 &&
+    input
+      .match(/Size (.*?), ULed/)[1]
+      .replace('MiB', 'MB')
+      .replace('GiB', 'GB')
+
+  const r =
+    input.match(/HDRip/i) ||
+    input.match(/BRRip/i) ||
+    input.match(/DVDRip/i) ||
+    input.match(/DVDScr/i) ||
+    input.match(/WED-DL/i) ||
+    input.match(/HDCAM/i) ||
+    input.match(/HD CAM/i) ||
+    input.match(/HD-TS/i) ||
+    input.match(/HD-TC/i) ||
+    input.match(/TSRip/i) ||
+    input.match(/HDTS/i) ||
+    input.match(/HD TS/i) ||
+    input.match(/HD TC/i)
+  const lowQuality =
+    input.match(/HDCAM/i) ||
+    input.match(/HD CAM/i) ||
+    input.match(/HD-TS/i) ||
+    input.match(/HD-TC/i) ||
+    input.match(/TSRip/i) ||
+    input.match(/HDTS/i) ||
+    input.match(/HD TS/i) ||
+    input.match(/HD TC/i)
+
+  const quality = r ? r.toString() : 'N/A'
+  const hd = !lowQuality
+  const qualityIndex = input.indexOf(quality)
+
+  const uploadedIndex = input.indexOf('Uploaded')
+  const sizeIndex = input.indexOf(' Size')
+  const endOfTitleIndex = uploadedIndex
+  const y =
+    input.match(/2015/) || input.match(/2016/) || input.match(/2017/) || input.match(/2018/) || input.match(/2019/)
+  const year = y ? y.toString() : new Date().getFullYear().toString()
+  const yearIndex = input.slice(0, endOfTitleIndex - 1).indexOf(year)
+
+  const title = input
+    .slice(0, endOfTitleIndex - 1)
+    .replace(/\./g, ' ')
+    .replace(/\n/g, '')
+    .replace(/\t/g, '')
+    .trim()
+
+  const endOfMovieTitle = yearIndex > 0 ? yearIndex : qualityIndex
+  const movieTitle = input
+    .slice(0, endOfMovieTitle - 1)
+    .replace(/\./g, ' ')
+    .replace(/\n/g, '')
+    .replace(/\t/g, '')
+    .trim()
+
+  const full = input
+    .replace(/\./g, ' ')
+    .replace(/\n/g, '')
+    .replace(/\t/g, '')
+    .trim()
+
+  const uploadedAtTime = input
+    .slice(uploadedIndex + 9, sizeIndex - 1)
+    .replace(/\./g, ' ')
+    .replace(/\n/g, '')
+    .replace(/\t/g, '')
+  const uploadedAt = parseLooseDate(uploadedAtTime).toISOString()
+
+  return {
+    quality,
+    size,
+    title,
+    movieTitle,
+    year,
+    uploadedAt,
+    full,
+    hd,
+  }
+}
+
+const parseLooseDate = uploadedAt => {
+  let timeSinceRelease
+  if (uploadedAt.includes('Today')) timeSinceRelease = moment()
+  else if (uploadedAt.includes('Y-day')) timeSinceRelease = moment().subtract(1, 'day')
+  else if (uploadedAt.includes(':')) timeSinceRelease = moment(uploadedAt, 'MM-DD HH:SS')
+  else timeSinceRelease = moment(uploadedAt, 'MM-DD YYYY')
+  return timeSinceRelease
+}
