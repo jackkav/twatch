3111f50af4af3401635b2cdb79f7529f802c7d50
Jack Kavanagh <jackkav@gmail.com>
2018-03-17 18:35:31 +0800

    trailers

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react'
 import shortid from 'shortid'
 import { pbParse } from './parsers'
-
+import { get } from 'lodash'
 import 'tachyons'
 
 class App extends Component {
@@ -59,7 +59,7 @@ class InnerRow extends Component {
     return (
       <div className="flex flex-wrap items-start">
         <div className="outline w-1 pa3">
-          <Trailer />
+          <Trailer movie={this.props.movie} />
         </div>
         <div className="outline w-1 pa3">
           <div>Released: {this.props.movie.uploadedAt.slice(0, 10)}</div>
@@ -107,13 +107,33 @@ class Trailer extends Component {
   state = {
     isFetching: false,
   }
+  async componentWillMount() {
+    this.setState({ isFetching: true })
+    const { icon, watch } = await getTrailer(this.props.movie.movieTitle, this.props.movie.year)
+    this.setState({ isFetching: false, icon, watch })
+  }
+
   render() {
     if (this.state.isFetching) return <div>loading</div>
     return (
-      <a target="blank" href="https://www.youtube.com/watch?v=2w5VZmos5I4">
-        <img src="https://i.ytimg.com/vi/2w5VZmos5I4/default.jpg" alt="yt" />
+      <a target="blank" href={this.state.watch}>
+        <img
+          src={this.state.icon || 'http://icons.iconarchive.com/icons/dtafalonso/android-lollipop/72/Youtube-icon.png'}
+          alt="yt"
+        />
       </a>
     )
+  }
+}
+const getTrailer = async (name, year) => {
+  const searchTerm = name + ' ' + year + ' trailer'
+  const url = `https://www.googleapis.com/youtube/v3/search?key=AIzaSyBjnMTlF9ou968qeDBc6LQpN860jJ0Juj0&q=${searchTerm}&part=snippet`
+  let f = await fetch(url)
+  let json = await f.json()
+  const first = json.items[0]
+  return {
+    icon: get(first, 'snippet.thumbnails.default.url', 'https://i.ytimg.com/vi/dQw4w9WgXcQ/default.jpg'),
+    watch: `https://www.youtube.com/watch?v=${get(first, 'id.videoId', 'dQw4w9WgXcQ')}`,
   }
 }
 const getOmdb = async (name, year) => {
